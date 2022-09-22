14bdcfc2bcae491ebda7f376fdc869ae81d3c0d0
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-09-22 05:09:59 -0500

    es8

---
+++ src/es7/array-includes.js
@@ -0,0 +1,6 @@
+let numbers = [1,2,3,4,5,6,7];
+
+console.log(numbers.includes(9));
+
+const list = ['rmc','dav','ana'];
+console.log(list.includes('rmc'));
---
+++ src/es8/trailing-commas.js
@@ -0,0 +1,3 @@
+const array = [15,16,17,18,25,26,44,50, , , , 22];
+console.log(array);
+console.log(array.length);
